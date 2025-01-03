b87aeccafe0ac2ee2250488f573b9221cf26d0f4
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-02 21:40:04 -0800

    make edsl internal

--- app/EDSL.hs
+++ app/EDSL.hs
@@ -1,4 +1,4 @@
-module EDSL(
+module EDSL (
     Html,
     Title,
     Structure,
@@ -6,63 +6,7 @@ module EDSL(
     p_,
     h1_,
     append_,
-    render
+    render,
 ) where
 
-newtype Html
-    = Html String
-
-newtype Structure
-    = Structure String
-
-type Title =
-    String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-    Html
-        ( el
-            "html"
-            ( el "head" (el "title" (escape title))
-                <> el "body" (getStructureString content)
-            )
-        )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content =
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-append_ :: Structure -> Structure -> Structure
-append_ c1 c2 =
-    Structure (getStructureString c1 <> getStructureString c2)
-
-getStructureString :: Structure -> String
-getStructureString content =
-    case content of
-        Structure str -> str
-
-render :: Html -> String
-render html =
-    case html of
-        Html str -> str
-
-
-escape :: String -> String
-escape =
-    let 
-        escapeChar c =
-            case c of
-                '<' -> "&lt;"
-                '>' -> "&gt;"
-                '&' -> "&amp;"
-                '"' -> "&quot;"
-                '\'' -> "&#39;"
-                _ -> [c]
-    in
-        concatMap escapeChar
+import EDSL.Internal
